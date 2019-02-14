91e023ca660a8a6518c3454aa75ddad0af641326
Sheyne Anderson <sheyne@sheyne.com>
2019-02-13 22:29:04 -0800

    ugly implementation of rook and bishop moves

--- src/Lib.hs
+++ src/Lib.hs
@@ -71,13 +71,39 @@ showBoard b = intercalate "\n" (map showRow (toList b))
 pieceAt :: Board -> (Int, Int) -> Square
 pieceAt b (r, c) = b `index` r `index` c
 
+isUnfilled :: Square -> Bool
+isUnfilled Unfilled = True
+isUnfilled _ = False
+
 isValidMove2 :: Board -> (Int, Int) -> (Int, Int) -> Square -> Square -> Bool
+isValidMove2 _ l1 l2 _ _
+                       | l1 == l2 = True
 isValidMove2 _ _ _ Unfilled _ = False
 isValidMove2 _ _ _ (Filled White _) (Filled White _) = False
 isValidMove2 _ _ _ (Filled Black _) (Filled Black _) = False
 isValidMove2 _ (r1, c1) (r2, c2) (Filled _ Knight) _ = let rd = abs (r1 - r2)
                                                            cd = abs (c1 - c2)
                                                         in (rd, cd) == (1, 2) || (rd, cd) == (2, 1)
+isValidMove2 b (r1, c1) ts@(r2, c2) p@(Filled _ Rook) finalLoc = (rd == 0 || cd == 0) 
+                                                                 && (newLoc == ts || (isUnfilled newSq
+                                                                                     && isValidMove2 b newLoc (r2, c2) p finalLoc))
+                                                            where rd = abs (r1 - r2)
+                                                                  cd = abs (c1 - c2)
+                                                                  rs = signum (r2 - r1) + r1
+                                                                  cs = signum (c2 - c1) + c1
+                                                                  newLoc = (rs, cs)
+                                                                  newSq = pieceAt b newLoc
+isValidMove2 b (r1, c1) ts@(r2, c2) p@(Filled _ Bishop) finalLoc = rd == cd
+                                                                   && (newLoc == ts || (isUnfilled newSq
+                                                                                       && isValidMove2 b newLoc (r2, c2) p finalLoc))
+                                                             where rd = abs (r1 - r2)
+                                                                   cd = abs (c1 - c2)
+                                                                   rs = signum (r2 - r1) + r1
+                                                                   cs = signum (c2 - c1) + c1
+                                                                   newLoc = (rs, cs)
+                                                                   newSq = pieceAt b newLoc
+
+
 
 isValidMove :: Board -> (Int, Int) -> (Int, Int) -> Bool
 isValidMove b l1 l2 = isValidMove2 b l1 l2 (b `pieceAt` l1) (b `pieceAt` l2)
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,3 +10,30 @@ main = hspec $
     it "can't move on self" $
         let board = putAt (2, 2) (Filled White Knight) $ putAt (3, 4) (Filled White Knight) unfilledBoard
             in isValidMove board (2, 2) (3, 4) `shouldBe` False
+    it "lets knights capture" $
+        isValidMove (putAt (3, 4) (Filled Black Knight) (putAt (2, 2) (Filled White Knight) unfilledBoard)) (2, 2) (3, 4) `shouldBe` True
+    it "only lets bishops move in diagonals" $ do
+        isValidMove (putAt (2, 2) (Filled White Bishop) unfilledBoard) (2, 2) (4, 4) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Bishop) unfilledBoard) (2, 2) (3, 4) `shouldBe` False
+        isValidMove (putAt (2, 2) (Filled White Bishop) unfilledBoard) (2, 2) (0, 4) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Bishop) unfilledBoard) (2, 2) (1, 3) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Bishop) unfilledBoard) (2, 2) (1, 3) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Bishop) unfilledBoard) (2, 2) (2, 3) `shouldBe` False
+    it "only lets rooks move straight" $ do
+        isValidMove (putAt (2, 2) (Filled White Rook) unfilledBoard) (2, 2) (2, 5) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Rook) unfilledBoard) (2, 2) (4, 2) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Rook) unfilledBoard) (2, 2) (0, 2) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Rook) unfilledBoard) (2, 2) (2, 1) `shouldBe` True
+        isValidMove (putAt (2, 2) (Filled White Rook) unfilledBoard) (2, 2) (1, 1) `shouldBe` False
+        isValidMove (putAt (2, 2) (Filled White Rook) unfilledBoard) (2, 2) (5, 1) `shouldBe` False
+        isValidMove (putAt (2, 2) (Filled White Rook) unfilledBoard) (2, 2) (3, 1) `shouldBe` False
+    it "doesn't let rooks move through pieces" $
+        isValidMove (putAt (2, 4) (Filled Black Knight) (putAt (2, 2) (Filled White Rook) unfilledBoard)) (2, 2) (2, 5) `shouldBe` False
+    it "allows rook captures" $
+        isValidMove (putAt (2, 4) (Filled Black Knight) (putAt (2, 2) (Filled White Rook) unfilledBoard)) (2, 2) (2, 4) `shouldBe` True
+    it "doesn't let bishops move through pieces" $
+        isValidMove (putAt (4, 4) (Filled Black Knight) (putAt (2, 2) (Filled White Bishop) unfilledBoard)) (2, 2) (5, 5) `shouldBe` False
+    it "allows bishop captures" $
+        isValidMove (putAt (4, 4) (Filled Black Knight) (putAt (2, 2) (Filled White Bishop) unfilledBoard)) (2, 2) (4, 4) `shouldBe` True
+        
+            
